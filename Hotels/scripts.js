65af3e46b88b8a3d7cffeb02aef0ba4d43911426
AliM-1997 <aliimortada797@gmail.com>
2024-07-14 19:16:08 +0300

    started by initialze like an object for manual testing the procedure of adding hotels

---
+++ Hotels/scripts.js
@@ -0,0 +1,34 @@
+const hotelsData = {
+  hotels: [
+    {
+      name: "Phoenicia Beirut",
+      imageUrl: "assets/phoneca_beirut.jpg",
+      location: "Beirut",
+      rating: 4.5,
+      services: [
+        "Restaurant",
+        "Hotel star",
+        "Free Parking",
+        "Free WiFi",
+        "Air Conditioning",
+        "Coffee Shop",
+        "Fitness Center",
+      ],
+    },
+    {
+      name: "Another Hotel",
+      imageUrl: "assets/another_hotel.jpg",
+      location: "Tripoli",
+      rating: 4.0,
+      services: ["Restaurant", "Free Parking", "Free WiFi"],
+    },
+    {
+      name: "Luxury Resort",
+      imageUrl: "assets/luxury_resort.jpg",
+      location: "Jounieh",
+      rating: 4.8,
+
+      services: ["Restaurant", "Spa", "Beach Access", "Pool", "Free WiFi"],
+    },
+  ],
+};
